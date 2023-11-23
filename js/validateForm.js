03139f41d7c4d305f0c108b449da767fc7670822
Mondot547 <alex.mondot.mail.pro@gmail.com>
2023-11-23 16:52:02 +0100

    my first commit

---
+++ js/validateForm.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("form"),
+    statusTxt = form.querySelector("#button-area span");
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+    statusTxt.style.display = "block";
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "message.php", true);
+    xhr.onload() = {
+        if(xhr.readyState == 4 && xhr.status == 200) {
+        let response = xhr.response;
+    }
+}
+xhr.send();
+}
